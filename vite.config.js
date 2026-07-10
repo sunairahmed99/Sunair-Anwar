38e62c3c2ffc4ac524fe1640569d82ebb526e7c0
Sunair Anwar <102017033+sunairahmed99@users.noreply.github.com>
2026-07-10 14:04:10 -0700

    Update profile image production config clean up extra files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })

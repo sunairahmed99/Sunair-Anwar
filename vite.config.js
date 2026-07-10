e1b59803a1134f0f4fce6e72524cce5f1652b0a1
Sunair Anwar <102017033+sunairahmed99@users.noreply.github.com>
2026-07-10 14:18:12 -0700

    Fix build issues

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   build: {
     sourcemap: false,
-    minify: 'esbuild',
-    esbuildOptions: {
-      drop: ['console', 'debugger'],
-    },
   },
 })
+
